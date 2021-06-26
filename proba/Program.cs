d117c761b3a4d54e94183e0299bb56fab6cb40a1
dijanalozanoska <didilence@hotmail.com>
2021-06-26 17:05:57 +0200

    his money on the side

--- proba/Program.cs
+++ proba/Program.cs
@@ -7,6 +7,8 @@ namespace proba
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
