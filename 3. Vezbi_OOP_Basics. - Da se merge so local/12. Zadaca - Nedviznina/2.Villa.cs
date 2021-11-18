3ed6f49bf8062bc2bbef15b326ef5a805fca91e3
dijanalozanoska <didilence@hotmail.com>
2021-11-18 22:56:22 +0100

    12_zadaca

---
+++ 3. Vezbi_OOP_Basics. - Da se merge so local/12. Zadaca - Nedviznina/2.Villa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Vila : Nedviznina
+{
+    public float DanokNaLuksuz { get; set; }
+
+    public Vila(string inputadresa, string inputkvadratura, string inputcenapokvadrat, string inputdanoknaluksuz)
+       : base(inputadresa, inputkvadratura, inputcenapokvadrat)
+    {
+        DanokNaLuksuz = float.Parse(inputdanoknaluksuz);
+    }
+
+
+    public override void Pecati()
+    {
+        base.Pecati();
+    }
+
+
+    public override void DanokNaImot()
+    {
+        float danokNaLuksuz = (0.05f + DanokNaLuksuz / 100) * (getKvadratura() * getCenaPoKvadrat());
+        Console.WriteLine($"Danok na imot za {Adresa} e: {danokNaLuksuz}");
+    }
+
+
+}
---
+++ 3. Vezbi_OOP_Basics. - Da se merge so local/12. Zadaca - Nedviznina/NedvizninaVoid.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vezbi_OOP_Basics
+{
+    public class ZadacaNedviznina
+    {
+        public static void NedvizninaVoid()
+        {
+            /*TODO: CONSOLE READLINE INPUT
+  
+            Kukja_vo_Centar
+            60
+            850
+            Vila_na_Vodno
+            110
+            1120
+            10
+
+            */
+
+            
+            Console.WriteLine("***   Input za Nedviznina   ****");
+            Console.Write("Vnesi Adresa: ");
+            string inputadresa = Console.ReadLine();
+            Console.Write("Vnesi Kvadratura: ");
+            string inputkvadratura = Console.ReadLine();
+            Console.Write("Vnesi cena po kvadrat: ");
+            string inputcenapokvadrat = Console.ReadLine();
+
+            var nedviznina = new Nedviznina(inputadresa, inputkvadratura, inputcenapokvadrat);
+            Console.WriteLine();
+            Console.WriteLine("***   Output za Nedviznina   ***");
+            nedviznina.Pecati();
+            nedviznina.DanokNaImot();
+
+            
+
+            Console.WriteLine("***   Output za Vila   ***");
+            Console.Write("Vnesi Adresa: ");
+            string inputadresa1 = Console.ReadLine();
+            Console.Write("Vnesi Kvadratura: ");
+            string inputkvadratura1 = Console.ReadLine();
+            Console.Write("Vnesi cena po kvadrat: ");
+            string inputcenapokvadrat1 = Console.ReadLine();
+            Console.Write("Vnesi ddv: ");
+            string inputdanoknaluksuz = Console.ReadLine();
+
+            var vila = new Vila(inputadresa1, inputkvadratura1, inputcenapokvadrat1, inputdanoknaluksuz);
+            Console.WriteLine();
+
+            Console.WriteLine("***   Output za Vila   ***");
+            vila.Pecati();
+            vila.DanokNaImot();
+
+        }
+    }
+}
