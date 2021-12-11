19d670cfafb50c9b3b217711796771fd73d3e359
dijanalozanoska <didilence@hotmail.com>
2021-12-11 14:36:44 +0100

    vezbi

---
+++ 3. Vezbi_OOP_Basics. - Da se merge so local/14. Zadaca - Doktor/1.Lekar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Lekar
+{
+    public string Ime { get; set; }
+
+    public string Prezime { get; set; }
+
+    public int Faksimil { get; set; }
+
+    public decimal PocetnaPlata { get; set; }
+
+    public Lekar(string ime, string prezime, int faksimil, decimal pocetnaplata)
+    {
+        Faksimil = faksimil;
+        Ime = ime;
+        Prezime = prezime;
+        PocetnaPlata = pocetnaplata;
+    }
+
+    public virtual void Pecati()
+    {
+        Console.WriteLine($"{Faksimil}: {Ime} {Prezime}");
+    }
+
+    public virtual decimal Plata()
+    {
+        return PocetnaPlata;
+    }
+}
---
+++ 3. Vezbi_OOP_Basics. - Da se merge so local/14. Zadaca - Doktor/LekarVoid.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vezbi_OOP_Basics
+{
+    public class ZadacaDoktor
+    {
+        public static void DoktorVoid()
+        {
+            Console.WriteLine("====== Testiranje na klasata lekar =======");
+            Console.Write("Vnesi broj na lekari: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var kotizacija = new List<decimal>();
+                MaticenLekar lekar;
+                Console.WriteLine($"Vnes na {i + 1} lekar");
+                Console.Write("Vnesi faksimil");
+                int faksimil = int.Parse(Console.ReadLine());
+                Console.WriteLine("Vnesi ime");
+                string ime = Console.ReadLine();
+                Console.WriteLine("Vnesi prezime");
+                string prezime = Console.ReadLine();
+                Console.WriteLine("Vnesi pocetna plata");
+                decimal pplata = decimal.Parse(Console.ReadLine());
+                int brPacienti = int.Parse(Console.ReadLine());
+                for (int j = 0; j < brPacienti; j++)
+                {
+                    Console.Write($"Vnesi {i + 1} kotizacija");
+                    kotizacija.Add(decimal.Parse(Console.ReadLine()));
+                }
+
+                lekar = new MaticenLekar(ime, prezime, faksimil, pplata, brPacienti, kotizacija);
+                lekar.Pecati();
+            }
+        }
+    }
+}
